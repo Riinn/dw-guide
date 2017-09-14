173311bf23fb1e87ba6caf4d2d17a51fb0d1a852
yunfei <harrygong1992@yahoo.com>
2017-09-15 09:29:08 +0930

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const dw = 466; //Demon's Wheel
 const bandersnatch = 46601; // Bandersnatch
 const demoros = 46602; //Demoros
 
+/*
+const dices = {
+	0: {0: 'Hit ALL', 1: 'Don\'t hit RED', 2: 'Hit RED'},	//red dice
+	1: {0: 'Hit ALL', 1: 'Don\'t hit BLUE', 2: 'Hit BLUE'},	//blue dice
+	2: {0: 'Hit ALL', 1: 'Don\'t hit WHITE', 2: 'Hit WHITE'}	//white dice
+};*/
 
 //Planned call outs: Bandersnatch: Stay in or Get out
 //Demoros: LASER
@@ -15,7 +21,8 @@ module.exports = function DWGuide(dispatch) {
 	let boss = null;
 	let enabled = true;
 	let sendToParty = false;
-	
+	let msg;
+	let orbit=0; //0: STOP, 1:counter-clockwise, 2:clockwise
 	
 	//Chat hooks and messages
 	const chatHook = event => {		
@@ -79,9 +86,8 @@ module.exports = function DWGuide(dispatch) {
 		}
 		if(boss) {
 			hp = boss.curHP/boss.maxHP;
-			if(hp<=0) {
-				boss = null;
-			}
+			if(hp<=0) boss = null;
+			if(hp<=0.3) orbit = 0;
 		}
 	});
 	
@@ -118,7 +124,7 @@ module.exports = function DWGuide(dispatch) {
 			if (event.skill==1171391577) {
 				sendMessage('<font color = "#ff3300">LASER!!!!!!</font>');
 			}
-			//1171391773 First Red Blue-inner explosion
+			//1171391773 First Blue Outer-inner explosion
 			//1171391774 First Red Outer-inner explosion
 			//1171391775 Blue Outer-inner explosion
 			//1171391776 Red Inner-outer explosion
@@ -132,6 +138,7 @@ module.exports = function DWGuide(dispatch) {
 			}
 			//1171391767 Red,Blue,White dice? mech
 			
+			
 			//1171391681 Blue circles, 3 times
 			//1171391687 Red circles, 3 times
 			if (event.skill==1171391687){
@@ -139,5 +146,28 @@ module.exports = function DWGuide(dispatch) {
 			}
 		}
 	});
+	/*
+	dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
+		if (!enabled || !boss) return;
+		if (boss.templateId == demoros){
+			msg = event.message;
+			systemMessage('Message : '+msg);
+		}
+	});*/
+	
+	dispatch.hook('S_SPAWN_NPC', 3, (event) => {
+		if(!enabled || !boss) return;
+		if(boss.templateId != demoros) return;
+		//46621 clockwise ball
+		//46622 counterclockwise ball
+		if(event.templateId == 46621){
+			systemMessage('DON\'t HIT THAT COLOR');
+			orbit = 1;
+		}
+		if(event.templateId == 46622){
+			systemMessage('HIT THAT COLOR');
+		}
+	});
 }
+
 
